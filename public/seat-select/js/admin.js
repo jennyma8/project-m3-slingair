7c268ae383fd2c23e81138c074dfa52df1961f57
jenny ma <jenny_lau_8@hotmail.com>
2020-07-08 22:17:07 -0400

    complete

--- public/seat-select/js/admin.js
+++ public/seat-select/js/admin.js
@@ -13,40 +13,25 @@ const getReservations = async(flightNum) => {
 }
 
 const displayTable = (event) => {
-    //create a function scope empty array to hold all reservations
   let reservations = [];
-
-  //obtain all current reservations and store them
   getReservations()
   .then(res => res.json())
   .then(data => data.forEach(res => {
     if(res.flight === flightInput.value) {
       reservations.push(res);
     }
-    
   }));
 
   //obtain all seating info based on the selected flight
   getSeats(flightInput.value)
   .then(res => res.json())
   .then(seats => {
-        //loop over every single seating object for the current flight
-    //to create the appropriate rows
     seats.forEach(seat => {
-
       //this will create the initial row element required
       let tableRow = document.createElement('tr');
 
-      //this is to check if there are any entered reservations, since seats are
-      //occupied even though there arent any reservations under it
-      //I want to render the table with the name of the person that reserved if
-      //if the seat is occupied but not reserved under any name, leave it blank
       if(reservations.length > 0) {
-
-        //loop over the reservation array at every seat
-        reservations.forEach(res => {
-
-          //This injects HTML into the <tr> element that was created for each row
+          reservations.forEach(res => {
           let seatOccupied = `<tr><td>${seat.id}</td><td>${seat.isAvailable}</td><td><a href="/seat-select/confirmed/${res.id}">${res.givenName + " " + res.surname}</a></td></tr>`
           let seatUnavailable = `<tr><td>${seat.id}</td><td>${seat.isAvailable}</td><td>${seat.isAvailable ? "-" : "Anonymous" }</td></tr>`
         
@@ -65,5 +50,4 @@ const displayTable = (event) => {
   })
 }
 
-//this will render the table for the chosen flight
 flightInput.addEventListener('blur', displayTable);
--- server.js
+++ server.js
@@ -3,23 +3,63 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const { flights } = require("./test-data/flightSeating");
+const { reservations } = require("./test-data/reservations");
+const { v4: uuidv4 } = require('uuid'); //new
 
 const PORT = process.env.PORT || 8000;
 
-//import route handling functions
-const { 
-  getFlights,
-  handleSeats,
-  addReservation,
-  handleConfirmation,
-  getReservations,
-  handleReservations,
-  adminView 
-} = require("./handlers");
+//return all seatings info of the flight
+const handleFlight = (req, res) => {
+  const { flightNumber } = req.params;
+  const allFlights = Object.keys(flights);
+  if(allFlights.includes(flightNumber)) {
+    res.status(200).send(flights[flightNumber]);
+  } else {
+    res.status(400).send({message: "Flight not Found"});
+  }
+};
 
-//this function will return all of the flight's seating data
-//if the flight exists
+//this will render the seat-select page with all possible flights
+const handleSeats = (req, res) => {
+  const flightNames = Object.keys(flights);
+  res.status(200).render("./pages/seat-select", { flights: flightNames });
+};
 
+//this handle confirmation pages needs to get the user flight id
+const handleConfirmation = (req,res) => {
+  let userId = req.params.id;
+  let userInfo = reservations.find(user => user.id === userId);
+  res.status(200).render("./pages/confirmed", { user: userInfo })
+}
+
+//push new reservation to database
+const addReservation = (req,res) => {
+  let info = req.body;
+  let newId = {id: uuidv4()};
+  let newData = {...newId, ...info}
+  reservations.push(newData);
+    //update isAvailable status from true to false
+  flights[info.flight].find(seat => seat.id === info.seat)
+    .isAvailable = false;
+}
+
+//get all reservations
+const getReservations = (req,res) => {
+  res.status(200).send(reservations);
+}
+
+const handleReservations = (req,res) => {
+  res.status(200).render('./pages/view-reservation')
+}
+
+//admin page
+const adminView = (req,res) => {
+  res.status(200).render('./pages/admin', {
+    flightNames: Object.keys(flights),
+    flights: flights,
+  })
+}
 
 express()
   .use(function (req, res, next) {
@@ -34,32 +74,16 @@ express()
   .use(express.static('public'))
   .use(bodyParser.json())
   .use(express.urlencoded({ extended: false }))
-  .set("view engine", "ejs")
+  .set("view engine", "ejs") //new
 
   // endpoints
- //renders seat-selection
   .get("/seat-select", handleSeats)
-
- //sends user to confirmation page
   .get("/seat-select/confirmed/:id", handleConfirmation)
-
- //get array of a specific flight
-  .get("/flights/:flightNumber", getFlights)
-
- //search for a reservation by #id
+  .get("/flights/:flightNumber", handleFlight)
   .get("/view-reservation", handleReservations)
-
- //receives form data and adds it to reservations
   .post("/users", addReservation)
-
- //retrieves array of all reservations
   .get("/users", getReservations)
-
- //admin view of all reservations
   .get("/admin", adminView)
-
-
-  
 
   .use("*", (req, res) => res.send('Not Found'))
   .listen(PORT, () => console.log(`Listening on port ${PORT}`));
